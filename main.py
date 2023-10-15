9102406bb5dd9dc818dfb8acdde3a165636188f8
Rohit <ghost@Rohits-MacBook-Air.local>
2023-10-15 14:24:14 +0530

    changes made

---
+++ main.py
@@ -0,0 +1,5 @@
+try:
+    from Chicken-Disease-Classification import logger
+except ImportError as e:
+    # Handle the ImportError exception here
+    print(f"An error occurred while importing 'logger': {e}")
